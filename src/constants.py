d73dbf56541f77718909e9597f3804cbab4d9d3e
Monica Turner <turnermon@gmail.com>
2023-03-02 18:12:16 +0100

    Floodscan + CCCM (#2)
    * Add floodscan processing
    
    * Add floodscan processing
    
    * Added chirps
    
    * Remove old text
    
    * Add CCCM comparison to floodscan
    
    * Fixed reqs
    
    * Added comments
    
    * Fixed last typo

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,12 @@
+import os
 from pathlib import Path
 
 from ochanticipy import create_custom_country_config
 
 iso3 = "yem"
+
+oap_data_dir = Path(os.environ["OAP_DATA_DIR"])
+
 country_config = create_custom_country_config(
     Path(__file__).parent.resolve() / "yem.yaml"
 )
