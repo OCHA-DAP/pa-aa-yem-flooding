52691aaef6afa27704a1ed8eee446a5a1176b04b
Zack  Arno <zackarno@gmail.com>
2023-05-08 10:37:41 +0300

    styler

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,3 @@
-
-
 # using this space to document output data sets used for other purposes
 
 
@@ -12,8 +10,8 @@ library(targets)
 library(sf)
 tar_load(high_risk_flood_stats_by_cod)
 
-st_write(high_risk_flood_stats_by_cod$adm1,dsn = "../../qgis_projects/aa_yem_flood.gpkg","yem_adm1_rev",append = T)
-st_write(high_risk_flood_stats_by_cod$adm2,dsn = "../../qgis_projects/aa_yem_flood.gpkg","yem_adm2_rev",append = T)
+st_write(high_risk_flood_stats_by_cod$adm1, dsn = "../../qgis_projects/aa_yem_flood.gpkg", "yem_adm1_rev", append = T)
+st_write(high_risk_flood_stats_by_cod$adm2, dsn = "../../qgis_projects/aa_yem_flood.gpkg", "yem_adm2_rev", append = T)
 
 
 
@@ -25,62 +23,68 @@ st_write(high_risk_flood_stats_by_cod$adm2,dsn = "../../qgis_projects/aa_yem_flo
 tar_load(hres_rolled_per_date_gen)
 tar_load(era_chirps_long)
 
-write_rds(hres_rolled_per_date_gen,"app/shinydata/hres_rolled_per_date_gen.rds")
-write_rds(era_chirps_long,"app/shinydata/era_chirps_long.rds")
+write_rds(hres_rolled_per_date_gen, "app/shinydata/hres_rolled_per_date_gen.rds")
+write_rds(era_chirps_long, "app/shinydata/era_chirps_long.rds")
 
 
-# chirps-gefs data extracted to hulls by Monica & Pauline -- will probably eventually make this 
+# chirps-gefs data extracted to hulls by Monica & Pauline -- will probably eventually make this
 # a target output to simplify project structure, but for now using the csv... when I make into _target I can delete
 # from here.
 
 # will turn this is to a target- but temporarily want to see how it looks in dashboard so putting here.
-chirps_gefs <- read_csv(file.path(Sys.getenv("AA_DATA_DIR"),
-                                  "public",
-                                  "processed",
-                                  "yem",
-                                  "chirps_gefs",
-                                  "chirps-gefs_daily_high_risk_hulls.csv"))
+chirps_gefs <- read_csv(file.path(
+  Sys.getenv("AA_DATA_DIR"),
+  "public",
+  "processed",
+  "yem",
+  "chirps_gefs",
+  "chirps-gefs_daily_high_risk_hulls.csv"
+))
 
 
 # renaming for consistency with other shiny files
-chirps_gefs <- chirps_gefs %>% 
-    rename(date_forecast_made= "time",
-           governorate_name = "gov") 
+chirps_gefs <- chirps_gefs %>%
+  rename(
+    date_forecast_made = "time",
+    governorate_name = "gov"
+  )
 
 # dates missing from chirps-gefs so merge the dates back in with "NA" values to take care
 # of any unexpected issues when plotting
 
 # make date sequence that I know includes missing dates in chirps-gefs
-date_seq_df <- expand_grid(date_forecast_made = seq(as_date("2000-01-01"),
-                                                 as_date("2021-05-01"),1),
-                        leadtime=1:10, governorate_name=c("Hajjah","Marib"))
+date_seq_df <- expand_grid(
+  date_forecast_made = seq(
+    as_date("2000-01-01"),
+    as_date("2021-05-01"), 1
+  ),
+  leadtime = 1:10, governorate_name = c("Hajjah", "Marib")
+)
 
 
 # get missing dates
-missing_from_chirps_gefs <- date_seq_df %>% 
-    filter(!date_forecast_made %in% chirps_gefs$date_forecast_made)
+missing_from_chirps_gefs <- date_seq_df %>%
+  filter(!date_forecast_made %in% chirps_gefs$date_forecast_made)
 
 # merge missing dates
-chirps_gefs_complete<- bind_rows(chirps_gefs, missing_from_chirps_gefs) %>% 
-    arrange(date_forecast_made)
+chirps_gefs_complete <- bind_rows(chirps_gefs, missing_from_chirps_gefs) %>%
+  arrange(date_forecast_made)
 
 # calculate rolling sums
-chirps_gefs_long <- chirps_gefs_complete %>% 
-    mutate(
-        date_forecast_predict =date_forecast_made+leadtime
-    ) %>% 
-    group_by(governorate_name,date_forecast_made) %>% 
-    arrange(governorate_name, date_forecast_predict) %>% 
-    mutate(
-        roll3 = rollsum(value,k=3,align = "right",fill=NA ),
-        roll5 = rollsum(value,k=5,align = "right",fill=NA ),
-        roll10 = rollsum(value,k=10,align = "right",fill=NA )
-    ) %>% 
-    ungroup() %>% 
-    rename(precip_daily= "value") %>% 
-    pivot_longer(cols = matches("precip_daily|roll*")) 
+chirps_gefs_long <- chirps_gefs_complete %>%
+  mutate(
+    date_forecast_predict = date_forecast_made + leadtime
+  ) %>%
+  group_by(governorate_name, date_forecast_made) %>%
+  arrange(governorate_name, date_forecast_predict) %>%
+  mutate(
+    roll3 = rollsum(value, k = 3, align = "right", fill = NA),
+    roll5 = rollsum(value, k = 5, align = "right", fill = NA),
+    roll10 = rollsum(value, k = 10, align = "right", fill = NA)
+  ) %>%
+  ungroup() %>%
+  rename(precip_daily = "value") %>%
+  pivot_longer(cols = matches("precip_daily|roll*"))
 
 # write out
-write_rds(chirps_gefs_long,"shinydata/chirps_gefs.rds")
-
-
+write_rds(chirps_gefs_long, "shinydata/chirps_gefs.rds")
--- exploration/smap_era_chirps_investigate.R
+++ exploration/smap_era_chirps_investigate.R
@@ -6,68 +6,68 @@ library(zoo)
 ee_Initialize()
 tar_load(zonal_stats_high_risk_hull)
 tar_load(era5_w_rolling)
-smap <- read_rds(file.path(Sys.getenv("AA_DATA_DIR"),"public","raw","yem","smap","smap_l4_soil_moisture_anomaly.rds")) %>% 
-    rename(mean = value)
+smap <- read_rds(file.path(Sys.getenv("AA_DATA_DIR"), "public", "raw", "yem", "smap", "smap_l4_soil_moisture_anomaly.rds")) %>%
+  rename(mean = value)
 
 smap_era_chirps_anoms <- bind_rows(
-    zonal_stats_high_risk_hull$precip_daily %>% 
+  zonal_stats_high_risk_hull$precip_daily %>%
     mutate(
-        source = "chirps",
-        date= as_date(date)
+      source = "chirps",
+      date = as_date(date)
     ),
-era5_w_rolling %>% 
-    rename(mean = era_precip_daily) %>% 
+  era5_w_rolling %>%
+    rename(mean = era_precip_daily) %>%
     mutate(
-        source= "ERA5"
+      source = "ERA5"
     ),
-smap %>% 
+  smap %>%
     mutate(
-        source= "SMAP"
+      source = "SMAP"
     )
-) %>% 
-    select(1:5) %>% 
-    group_by(source, governorate_name) %>% 
-    arrange(date) %>% 
-    mutate(
-        roll30 = rollmean(mean,k=30,align = "center",fill= NA)
-    ) %>% 
-    mutate(
-        doy=lubridate::yday(date)
-    ) %>% 
-    group_by(doy,.add=T) %>% 
-    mutate(
-        mean30 = mean(roll30,na.rm = T),
-        diff_anom = roll30-mean30
-    ) %>% 
-    ungroup() 
-    
-    
-smap_era_chirps_anoms %>% 
-    pivot_wider(id_cols = c("governorate_name","date"),names_from =source, values_from= diff_anom) %>% 
-    ggplot(aes(x= chirps, y= lag(SMAP,0)))+
-    geom_point()
-
-smap_era_chirps_anoms %>% 
-    pivot_wider(id_cols = c("governorate_name","date"),names_from =source, values_from= diff_anom) %>% 
-    ggplot(aes(x= ERA5, y= lag(SMAP,0)))+
-    geom_point()
+) %>%
+  select(1:5) %>%
+  group_by(source, governorate_name) %>%
+  arrange(date) %>%
+  mutate(
+    roll30 = rollmean(mean, k = 30, align = "center", fill = NA)
+  ) %>%
+  mutate(
+    doy = lubridate::yday(date)
+  ) %>%
+  group_by(doy, .add = T) %>%
+  mutate(
+    mean30 = mean(roll30, na.rm = T),
+    diff_anom = roll30 - mean30
+  ) %>%
+  ungroup()
 
 
-smap_era_chirps_anoms %>% 
-    group_by(governorate_name, source,date=floor_date(date,"month")) %>% 
-    summarise(
-        diff_anom = mean(diff_anom,na.rm = T)
-    ) %>% 
-    ggplot(aes(x=date,y=diff_anom,color=source,group=source))+
-    geom_line(alpha=0.4)
+smap_era_chirps_anoms %>%
+  pivot_wider(id_cols = c("governorate_name", "date"), names_from = source, values_from = diff_anom) %>%
+  ggplot(aes(x = chirps, y = lag(SMAP, 0))) +
+  geom_point()
 
-smap_era_chirps_anoms %>% 
-    mutate(
-        diff_anom = ifelse(source=="SMAP",diff_anom*10,diff_anom)
-    ) %>% 
-    filter(date>"2017-01-01") %>% 
-    ggplot(aes(x=date,y=diff_anom,color=source,group=source))+
-    geom_line(alpha=0.4)+
-    facet_wrap(~governorate_name)+
-    labs(y="Anomaly (difference)")+
-    theme_hdx()
+smap_era_chirps_anoms %>%
+  pivot_wider(id_cols = c("governorate_name", "date"), names_from = source, values_from = diff_anom) %>%
+  ggplot(aes(x = ERA5, y = lag(SMAP, 0))) +
+  geom_point()
+
+
+smap_era_chirps_anoms %>%
+  group_by(governorate_name, source, date = floor_date(date, "month")) %>%
+  summarise(
+    diff_anom = mean(diff_anom, na.rm = T)
+  ) %>%
+  ggplot(aes(x = date, y = diff_anom, color = source, group = source)) +
+  geom_line(alpha = 0.4)
+
+smap_era_chirps_anoms %>%
+  mutate(
+    diff_anom = ifelse(source == "SMAP", diff_anom * 10, diff_anom)
+  ) %>%
+  filter(date > "2017-01-01") %>%
+  ggplot(aes(x = date, y = diff_anom, color = source, group = source)) +
+  geom_line(alpha = 0.4) +
+  facet_wrap(~governorate_name) +
+  labs(y = "Anomaly (difference)") +
+  theme_hdx()
