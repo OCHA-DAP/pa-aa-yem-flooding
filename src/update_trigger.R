8e463c203fe27abb50ddb05d57f300f0bb6e7a3c
Zack  Arno <zackarno@gmail.com>
2023-05-18 18:03:46 +0300

    script to update trigger data

---
+++ src/update_trigger.R
@@ -0,0 +1,23 @@
+library(lubridate)
+library(tidyverse)
+library(exactextractr)
+library(sf)
+library(terra)
+library(googledrive)
+source("R/load_chirps_gefs.R")
+
+
+# authorize drive access
+drive_auth(
+    path = Sys.getenv("YEM_TRIG_MONITORNG_JSON")
+)
+
+# load AOI
+aoi_drive <- drive_get(id = "1-2JIxDekilSor0YoNySLUdfTfQ9JgQRH")
+drive_download(aoi_drive,path = f <- tempfile(fileext = ".rds"))
+aoi <- read_rds(f)
+
+boom <- load_chirps_gefs_cropped(run_date = Sys.Date(),
+                                 leadtime=1,
+                                 mask=aoi,
+                                 write_outputs = T)
