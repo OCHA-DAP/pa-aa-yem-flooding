d302437afa21a6eb88bdef9032723ebe49db0bf1
Zack  Arno <zackarno@gmail.com>
2023-05-18 15:19:32 +0300

    rm googledrive.R

--- R/load_chirps_gefs.R
+++ R/load_chirps_gefs.R
@@ -1,100 +1,30 @@
-
-#' Title
-#'
-#' @param run_date 
-#' @param n_days 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-#' @import lubridate
-chirps_gefs_urls <- function(run_date=Sys.Date(),n_days=10){
-    
-    # add stopifnot/asserthat
-    
-    
-    base_url <- "https://data.chc.ucsb.edu/products/EWX/data/forecasts/CHIRPS-GEFS_precip_v12/daily_16day/"
-    forecast_dir <- format(run_date,"%Y/%m/%d")
-    daily_url <- paste0(base_url,forecast_dir)
-    
-    
-    start_date <- run_date
-    end_date <- start_date+n_days
-    forecasted_dates <- seq(start_date,end_date,by="day")
-    
-    file_names <- paste0("data.",format(forecasted_dates,"%Y.%m%d"),'.tif')
-    paste0(daily_url,"/",file_names)
-    
-    
-    
-}
-
-set_chirps_gefs_download_path <-  function(run_date=Sys.Date()){
-    
-}
-#' Title
-#'
-#' @param run_date 
-#' @param n_days 
-#' @param mask 
-#' @param ouput_dir 
-#'
-#' @return
-#' @export
-#' @import terra
-#' @examples
-#' library(tidyverse)
-#' library(terra)
-#' library(readr)
-#' library(sf)
-#' library(exactextractr)
-#' library(lubridate)
-#' roi_fp <- file.path(Sys.getenv("AA_DATA_DIR"),"public","processed","yem","live_monitoring","inputs", "high_risk_hulls.rds")
-#' roi <- read_rds(roi_fp)
-#' load_chirps_gefs_cropped(run_date = Sys.Date()-1,leadtime=1:2, mask=roi,write_outputs = T)
-
-
-
 load_chirps_gefs_cropped <- function(run_date=Sys.Date(),
                              leadtime=1:10,
                              mask=roi,
                              write_outputs=T
                              ){
     
-    
-    monitoring_drive_id <- Sys.getenv("AA_YEM_LIVE_MONITORING_DIR_ID")
-    gdrive_id_day <- cascade_ymd_directories(run_date = run_date,gdrive_id = monitoring_drive_id)
-    # need to adjust all gdrive handling
-    # gdrive_dir <- file.path(Sys.getenv("AA_DATA_DIR"),
-    #           "public",
-    #           "processed",
-    #           "yem",
-    #           "live_monitoring"
-    #           )
+    drive_contents <- drive_ls(
+        corpus="user"
+    ) 
+    r_dir <- drive_contents %>% 
+        filter(name=="chirps_gefs_rasters")
+    zonal_stats_dir <- drive_contents %>% 
+        filter(name=="chirps_gefs_zonal")
     
     base_url <- "https://data.chc.ucsb.edu/products/EWX/data/forecasts/CHIRPS-GEFS_precip_v12/daily_16day/"
     forecast_dir_url <- format(run_date,"%Y/%m/%d")
-    
-    # create folder for files if it does not exist
-    # gdrive_raster_outdir <- file.path(gdrive_dir,"inputs","chirps_gefs",forecast_dir_url)
-    # gdrive_processed_outdir <- file.path(gdrive_dir,"outputs","chirps_gefs")
-    # 
-    # if(!dir.exists(gdrive_raster_outdir)){
-    #     dir.create(gdrive_raster_outdir,recursive = T)
-    # }
     
     url_dir <- paste0(base_url,forecast_dir_url) 
     
     leadtime_adj <- leadtime-1
     forecasted_dates <- run_date+leadtime_adj
     
-    
     lyr_names <- paste0(format(run_date,"%Y-%m-%d"),".",leadtime)
-    gdrive_file_name <-   paste0(format(forecasted_dates,"%Y.%m%d"),'.tif')
-    # gdrive_file_path <-  file.path(gdrive_raster_outdir, paste0(format(forecasted_dates,"%Y.%m%d"),'.tif'))
+    gdrive_file_name <- paste0("yem_aoi_chirps_gefs_",lyr_names,".tif")
     
-    url_file_names <- paste0("data.",gdrive_file_name)
+    
+    url_file_names <- paste0("data.",format(forecasted_dates,"%Y.%m%d"),'.tif')
     url_downloads <- paste0(url_dir,"/",url_file_names)
     
     r_cropped_list <- url_downloads %>% 
@@ -120,7 +50,7 @@ load_chirps_gefs_cropped <- function(run_date=Sys.Date(),
             map2(gdrive_file_name,\(temp_path,fname){
                 cat("Uploading ",fname, " to GDRIVE")
                 drive_upload(media=temp_path,
-                             path=as_id(gdrive_id_day$id),
+                             path=as_id(r_dir$id),
                              name= fname)
             }
                 
@@ -157,7 +87,9 @@ load_chirps_gefs_cropped <- function(run_date=Sys.Date(),
     if(write_outputs){
         write_csv(roi_means,
                   file = temp_csv_file)
-        drive_upload(media= roi_means,path = as_id(get_gdrive_shared_dir(gdrive_id = monitoring_drive_id,which="outputs")$id),temp_csv_name)
+        drive_upload(media= temp_csv_file,
+                     path = as_id(zonal_stats_dir$id),
+                     temp_csv_name)
                   
     }
     return(roi_means)
--- src/update_trigger.R
+++ src/update_trigger.R
@@ -5,8 +5,7 @@ library(sf)
 library(terra)
 library(googledrive)
 source("R/load_chirps_gefs.R")
-source("R/googledrive.R")
-
+# source("R/googledrive.R")
 
 
 # authorize drive access
@@ -14,15 +13,12 @@ drive_auth(
     path = Sys.getenv("YEM_TRIG_MONITORNG_JSON")
 )
 
+# load AOI
+aoi_drive <- drive_get(id = "1-2JIxDekilSor0YoNySLUdfTfQ9JgQRH")
+drive_download(aoi_drive,path = f <- tempfile(fileext = ".rds"))
+aoi <- read_rds(f)
 
-# need to change these to googledrive functions
-roi_fp <- file.path(Sys.getenv("AA_DATA_DIR"),"public","processed","yem","live_monitoring","inputs", "high_risk_hulls.rds")
-roi <- read_rds(roi_fp)
-
-
-
-# I don't get why terra::rast() sometimes takes so long to download urls
 boom <- load_chirps_gefs_cropped(run_date = Sys.Date(),
                                  leadtime=1,
-                                 mask=roi,
+                                 mask=aoi,
                                  write_outputs = T)
