1d736ef428dc11041af2465cb3eebbda275fcf86
Zack  Arno <zackarno@gmail.com>
2023-05-22 11:47:42 +0300

    clean up documentation/roxygen and set CRON time for 910 AM utc

--- R/load_chirps_gefs.R
+++ R/load_chirps_gefs.R
@@ -1,19 +1,46 @@
 #' load_chirps_gefs_cropped
 #' @description
-#' the `load_chirps_gefs_cropped` function is used to get the inputs required to monitor the forecast data available
+#' the `load_chirps_gefs_cropped` function is used to get the inputs required to monitor the available chirps-gefs forecast data
 #' for the pilot AA Yemen trigger. The function extracts CHIRPS-GEFS data produced for any given day. This will
 #' be set on a CRON job to run just after the data is uploaded to: https://data.chc.ucsb.edu/products/EWX/data/forecasts/CHIRPS-GEFS_precip_v12/daily_16day/.
+#' The function is utilized in `src/update_trigger.R` which is the script which is set to run as a GH actions workflow.
 #'
-#' @param run_date \{character} date of run in YYYY-MM-DD format (default = Sys.Date())
-#' @param leadtime \{integer} vector indicating all leadtimes to download data for (default= c(1:10)),
+#' @param run_date \code{character} date of run in YYYY-MM-DD format (default = Sys.Date())
+#' @param leadtime \code{integer} vector indicating all leadtimes to download data for (default= c(1:10)),
 #'   leadtime 1 = 24 hours.
 #' @param mask spatial file in wgs84 projection to use for cropping rasters and calculating zonal stats
 #' @param write_outputs \code{logical} if T (default) write outputs to gdrive.
-#'
 #' @return
 #' 1. CHIRPS-GEFS raster tifs clipped to mask bounding box.
-#' 2. csv containing zonal mean for each raster
-#' @examples
+#' 2. Zonal mean by governorate for each raster as in memory object and csv.
+#' 
+#' @examples \dontrun{
+#' library(lubridate)
+#' library(dplyr)
+#' library(readr)
+#' library(tidyr)
+#' library(exactextractr)
+#' library(sf)
+#' library(terra)
+#' library(purrr)
+#' library(googledrive)
+#' source("R/load_chirps_gefs.R")
+#' # authorize drive access
+#' drive_auth(
+#'     path = Sys.getenv("YEM_TRIG_MONITORNG_JSON")
+#'     )
+#' # load AOI
+#' aoi_drive <- drive_get(id = "1-2JIxDekilSor0YoNySLUdfTfQ9JgQRH")
+#' drive_download(aoi_drive, path = f <- tempfile(fileext = ".rds"))
+#' aoi <- read_rds(f)
+#' chirps_gefs_zonal <- load_chirps_gefs_cropped(
+#'     run_date = Sys.Date(),
+#'     leadtime = c(1:10),
+#'     mask = aoi,
+#'     write_outputs = T
+#'     )
+#' }
+
 load_chirps_gefs_cropped <- function(run_date = Sys.Date(),
                                      leadtime = 1:10,
                                      mask = roi,
@@ -22,23 +49,27 @@ load_chirps_gefs_cropped <- function(run_date = Sys.Date(),
   drive_contents <- drive_ls(
     corpus = "user"
   )
+  # will store tifs here.
   r_dir <- drive_contents %>%
     filter(name == "chirps_gefs_rasters")
+  
+  # will store zonal mean csvs here
   zonal_stats_dir <- drive_contents %>%
     filter(name == "chirps_gefs_zonal")
-
 
   # get chirps gefs url paths
   base_url <- "https://data.chc.ucsb.edu/products/EWX/data/forecasts/CHIRPS-GEFS_precip_v12/daily_16day/"
   forecast_dir_url <- format(run_date, "%Y/%m/%d")
   url_dir <- paste0(base_url, forecast_dir_url)
 
-  leadtime_adj <- leadtime - 1 #
+  leadtime_adj <- leadtime - 1 # chirps-gefs stores first 24 hours forecast as Sys.Date()
   forecasted_dates <- run_date + leadtime_adj
-
+  
+  # format layer names for simple zonal mean extraction later
   lyr_names <- paste0(format(run_date, "%Y-%m-%d"), ".", leadtime)
+  
+  # output file names
   gdrive_file_name <- paste0("yem_aoi_chirps_gefs_", lyr_names, ".tif")
-
 
   url_file_names <- paste0("data.", format(forecasted_dates, "%Y.%m%d"), ".tif")
   url_downloads <- paste0(url_dir, "/", url_file_names)
@@ -70,10 +101,11 @@ load_chirps_gefs_cropped <- function(run_date = Sys.Date(),
         )
       })
   }
-
+  
   cat("stacking raster collection \n")
-  r_stack <- rast(r_cropped_list)
-
+  r_stack <- rast(r_cropped_list) # can stack and run zonal mean on entire stack
+  
+  # zonal means
   roi_means <- exact_extract(
     x = r_stack,
     y = mask,
--- src/update_trigger.R
+++ src/update_trigger.R
@@ -19,9 +19,23 @@ aoi_drive <- drive_get(id = "1-2JIxDekilSor0YoNySLUdfTfQ9JgQRH")
 drive_download(aoi_drive, path = f <- tempfile(fileext = ".rds"))
 aoi <- read_rds(f)
 
-boom <- load_chirps_gefs_cropped(
-  run_date = Sys.Date() - 1,
-  leadtime = 1,
+
+# extract chirps-gefs -----------------------------------------------------
+# get zonal mean by governorate for each leadtime
+# will use output object in memory for next processing steps
+# but raster tifs and zonal stats are also written to gdrive.
+chirps_gefs_zonal <- load_chirps_gefs_cropped(
+  run_date = Sys.Date(),
+  leadtime = c(1:10),
   mask = aoi,
   write_outputs = T
 )
+
+
+# rolling sums ------------------------------------------------------------
+
+
+
+# viz ---------------------------------------------------------------------
+
+
