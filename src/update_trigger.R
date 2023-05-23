8ff3571859e701d6ece9d37fd8a4b702bbd53a28
Zack  Arno <zackarno@gmail.com>
2023-05-23 10:01:39 +0300

    adj pipeline so gdrive locations are inputs into `load_chirps_gefs()`

--- src/update_trigger.R
+++ src/update_trigger.R
@@ -7,6 +7,7 @@ library(sf)
 library(terra)
 library(purrr)
 library(googledrive)
+library(blastula)
 source("R/load_chirps_gefs.R")
 
 # authorize drive access
@@ -14,28 +15,84 @@ drive_auth(
   path = Sys.getenv("YEM_TRIG_MONITORNG_JSON")
 )
 
-# load AOI
-aoi_drive <- drive_get(id = "1-2JIxDekilSor0YoNySLUdfTfQ9JgQRH")
+
+# access necessary GDRIVE paths -------------------------------------------
+# get gdrive directories
+all_drib <- drive_ls(
+    corpus = "user"
+)
+# will store tifs here.
+r_drib <- all_drib %>%
+    filter(name == "chirps_gefs_rasters")
+
+# will store zonal mean csvs here
+zonal_stats_drib <- all_drib %>%
+    filter(name == "chirps_gefs_zonal")
+
+# where AOI file is (doesn't change)
+aoi_drib <- drib %>% 
+    filter(name=="high_risk_hulls.rds")
+
+# load AOI rds as sf class
+aoi_drive <- drive_get(id = aoi_drib$id)
 drive_download(aoi_drive, path = f <- tempfile(fileext = ".rds"))
 aoi <- read_rds(f)
 
+
+# add some archiving operations?
 
 # extract chirps-gefs -----------------------------------------------------
 # get zonal mean by governorate for each leadtime
 # will use output object in memory for next processing steps
 # but raster tifs and zonal stats are also written to gdrive.
-chirps_gefs_zonal <- load_chirps_gefs_cropped(
+
+
+gefs_process_time <- system.time(
+  gefs_processed <- load_chirps_gefs_cropped(
   run_date = Sys.Date(),
   leadtime = c(1:10),
   mask = aoi,
-  write_outputs = T
+  write_outputs = T,
+  raster_drive = r_drib,
+  zonal_drive = zonal_stats_drib
+)
 )
 
 
-# rolling sums ------------------------------------------------------------
+chirps_gefs_zonal <- gefs_processed$zonal_means
+chirps_gefs_rstack <-  gefs_processed$raster_stack
+
+# email:
+
+# Load in e-mail credentials
+email_creds <- creds_envvar(
+    user = Sys.getenv("CHD_DS_EMAIL_USERNAME"),
+    pass_envvar = "CHD_DS_EMAIL_PASSWORD",
+    host = Sys.getenv("CHD_DS_HOST"),
+    port = Sys.getenv("CHD_DS_PORT"),
+    use_ssl = TRUE
+)
 
 
+# load in recipients
 
-# viz ---------------------------------------------------------------------
+receps_drive <- drive_get(id="10PkgaVJZhJIjoOd_31P55UWcRcZzS0Zo")
+drive_download(receps_drive, path = f <- tempfile(fileext = ".csv"))
+df_recipients <- read_csv(f)
 
+
+render_email(input = file.path(
+    "src",
+    "email",
+    "email.rmd"
+    ),
+    envir = parent.frame()
+    ) %>% 
+    smtp_send(
+        to = df_recipients$to,
+        # bcc = filter(df_recipients, !to)$email,
+        from = "data.science@humdata.org",
+        subject = paste0("Email Test: Yemen AA Rainfall Forecast Monitoring (",Sys.Date(),")"),
+        credentials = email_creds
+    )
 
