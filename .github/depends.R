153904243abfd5bab08fc73bd200e94e106cfd0b
Zack  Arno <zackarno@gmail.com>
2023-05-23 11:52:02 +0300

    add GITHUB_PAT to yaml and change order of package installation

--- .github/depends.R
+++ .github/depends.R
@@ -1,3 +1,6 @@
+install.packages("remotes") # moving this to the top to see if it will cause the GH action to fail faster
+remotes::install_github("OCHA-DAP/gghdx")
+
 install.packages(c("dplyr",
                    "ggplot2",
                    "readr",
@@ -12,11 +15,11 @@ install.packages(c("dplyr",
                    "here", # set paths in rmds without thinking.
                    "blastula", # rmd emails
                    "glue",  # mainly for pasting text in email.
-                   "remotes", # needed to install gghdx
+                   # "remotes", # needed to install gghdx
                    "showtext" # needed to set fonts in gghdx
                    ))
 
-remotes::install_github("OCHA-DAP/gghdx")
+
 
 
 # gdrive_2_0_url <- "https://cran.r-project.org/src/contrib/Archive/googledrive/googledrive_2.0.0.tar.gz"
