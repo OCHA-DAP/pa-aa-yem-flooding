12a38d5801e0f41173e36bd58822f191b8258f0f
Zack  Arno <zackarno@gmail.com>
2023-05-23 10:09:37 +0300

    tmp changes to shorten workflow to see if workflow runs on GH actions

--- src/update_trigger.R
+++ src/update_trigger.R
@@ -49,8 +49,8 @@ aoi <- read_rds(f)
 
 gefs_process_time <- system.time(
   gefs_processed <- load_chirps_gefs_cropped(
-  run_date = Sys.Date(),
-  leadtime = c(1:10),
+  run_date = Sys.Date()-1, # temp change
+  leadtime = c(1), #temp change
   mask = aoi,
   write_outputs = T,
   raster_drive = r_drib,
